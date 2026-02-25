b01fc284abf512f6c769fccc5d8a226de2dc8501
mustafacanakdogan <42907664+mustafacanakdogan@users.noreply.github.com>
2026-02-26 00:23:07 +0300

    feat(observability): add slow request demo and dashboards (#14)

---
+++ app/Http/Controllers/V2/SlowRequestDemoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\V2;
+
+use App\Http\Controllers\Controller;
+use App\Http\Responses\ApiResponse;
+use Illuminate\Http\Request;
+
+class SlowRequestDemoController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $ms = (int) $request->query('sleep_ms', 1200);
+        $ms = max(0, min($ms, 5000));
+
+        usleep($ms * 1000);
+
+        return ApiResponse::success([
+            'slept_ms' => $ms,
+        ]);
+    }
+}
