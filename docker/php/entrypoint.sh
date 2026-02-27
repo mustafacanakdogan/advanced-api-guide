98f2c2831e015468a609e5a2fe6fe54902ec93aa
mustafacanakdogan <42907664+mustafacanakdogan@users.noreply.github.com>
2026-02-27 20:48:52 +0300

    fix: compose install added to entrypoint sh (#23)

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-if [ -z "${APP_KEY:-}" ]; then
+if [ ! -f /var/www/html/vendor/autoload.php ]; then
+  echo "vendor not found, running composer install..."
+  composer install --no-interaction --prefer-dist --optimize-autoloader
+fi
+
+if ! grep -q '^APP_KEY=base64:' /var/www/html/.env 2>/dev/null; then
   echo "APP_KEY is missing, generating..."
   php artisan key:generate --force
 else
   echo "APP_KEY already exists, skipping key generation."
 fi
+
 
 echo "Running migrations..."
 php artisan migrate --force
